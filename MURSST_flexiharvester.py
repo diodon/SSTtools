e14aa0683a726d08ef52760e3b236453f88e0dff
Eduardo Klein <ekleins@gmail.com>
2020-01-08 19:00:26 +1100

    MURSST harvester

---
+++ MURSST_flexiharvester.py
@@ -0,0 +1,68 @@
+import sys
+import argparse
+
+import pandas as pd
+import urllib
+
+
+def makeRange(rangeValue):
+    rangeValue = str(rangeValue)
+    rr = '[(' + rangeValue + '):(' + rangeValue + ')]'
+    return rr
+
+
+def makeDateRange(date_start, date_end):
+    rr = '[(' + date_start + '):(' + date_end + ')]'
+
+    return rr
+
+
+def getDHW(latitude, longitude, date_start, date_end, fout):
+    """
+    function to harvest DHW and related variables from CRW ERDDAP server
+    the results are stores in a csv file
+    Eduardo Klein. ekleins@gmail.com
+
+    :param latitude: latitude in decimal degrees
+    :param longitude: longitude in decimal degrees
+    :param date_start: start date in yyyy-mm-dd
+    :param date_end: end date in yyyy-mm-dd
+    :param fout: file name for the results
+    :return: nothing
+    """
+
+    serverURL = 'https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41.csv?'
+    varNames = ['analysed_sst', 'analysis_error', 'mask', 'sea_ice_fraction']
+    varNames_DHW = ['time',  'latitude', 'longitude']
+
+    dateRange = makeDateRange(date_start, date_end)
+    constrains = urllib.parse.quote(dateRange + makeRange(latitude) + makeRange(longitude))
+    varList = varNames[0] + constrains
+    for var in varNames[1:]:
+        varList = varList + "," + var + constrains
+    url = serverURL + varList
+    ##print(url)
+    try:
+        DHW = pd.read_csv(url)
+        ## DHW = DHW.iloc[1:, ]     ##remove units row
+    except:
+        print("Failed")
+
+    DHW.to_csv(fout, index=None)
+
+    return print('Write {} SST values into {}'.format(len(DHW), fout))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Harvest MURSST and related variables from NASA-JPL ERDDAP server. The results are stored in a csv file')
+    parser.add_argument('-lat', dest='latitude', help='latitude in decimal degrees. Southern hemisphere negative', required=True)
+    parser.add_argument('-lon', dest='longitude', help='longitude in decimal degrees. Western hemisphere negative', required=True)
+    parser.add_argument('-from', dest='date_start', help='start date in yyyy-mm-dd', required=True)
+    parser.add_argument('-to', dest='date_end', help='end date in yyyy-mm-dd', required=True)
+    parser.add_argument('-fout', dest='fout', help='name of the output CSV file. Default SSToutput.csv',
+                        default='SSToutput.csv', required=False)
+    args = parser.parse_args()
+
+    getDHW(args.latitude, args.longitude, args.date_start, args.date_end, args.fout)
+
+
